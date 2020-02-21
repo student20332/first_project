e1914f894c194dab04cbaaec283669116771522a
student20332 <prominecraftik@gmail.com>
2020-02-21 17:01:19 +0300

    first_lesson

---
+++ app/Franction.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.lesson_1
+{
+    class Fraction
+    {
+        int sign;
+        int intpart;
+        int numerator;
+        int denominator;
+
+
+        public Fraction()
+        {
+            this.sign = 1;
+            this.intpart = 0;
+            this.numerator = 0;
+            this.denominator = 1;
+        }
+
+        public Fraction(int n, int d, int i = 0, int s = 1)
+        {
+            this.sign = s;
+            this.intpart = i;
+            this.numerator = n;
+            this.denominator = d;
+            this.GetMixedView();
+        }
+
+        void GetMixedView()
+        {
+            this.GetIntPart();
+            this.CancelLation();
+        }
+        void CancelLation()
+        {
+            if (this.numerator != 0) {
+                int m = this.denominator,
+                    n = this.numerator,
+                    ost = m%n;
+
+                while (ost != 0) {
+                    m = n;
+                    n = ost;
+                    ost = m % n;
+                }
+
+                int nod = n;
+
+                if (nod != 1) {
+                    this.numerator /= nod;
+                    this.denominator /= nod;
+                }
+            }
+        }
+        void GetIntPart()
+        {
+            if (this.numerator >= denominator) {
+                this.intpart += this.numerator / this.denominator;
+                this.numerator %= this.denominator;
+            }
+        }
+
+        public ~Fraction()
+        {
+            Console.WriteLine("Дробь" + this + "уничтожена!");
+        }
+    }
+}
--- app/app/Program.cs
+++ app/app/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using app.lesson_1;
 
 namespace app
 {
-    class Program
+    class App
     {
+
         static void Main(string[] args)
         {
+            Fraction qwe = new Fraction(3, 2);
+            Fraction qwe2 = new Fraction(3, 2);
+            Fraction qwe3 = new Fraction(3, 2);
+            Fraction qwe4 = new Fraction(3, 2);
         }
+        
     }
 }
